2ec2696f495cf6422083b3c619088577211792c2
MohamedBellagha <bellaghamohamed@gmail.com>
2021-01-31 11:13:29 +0100

    an updated commit message

--- django_likes/settings.py
+++ django_likes/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h#3i^#q7w=)-ht$93_8^trv8ntnoo!n)l2#)s46bzh6t@f&q@7'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', "test_secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
